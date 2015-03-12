e585419d972f5dbebf9c99fae9d1a730cc826c06
Alex Jose <aljonjose@gmail.com>
2015-03-13 01:43:03 +0800

    Made some javascript for login

---
+++ js/loginStore.js
@@ -0,0 +1,14 @@
+var userList = new Array();
+
+function User(name, email, pass, nick)
+{ 
+  var name;
+  var email;
+  var password;
+  var nickname;
+}
+
+function addUserToList()
+{
+  var nameofUser = document.getElementByName("email");
+}
